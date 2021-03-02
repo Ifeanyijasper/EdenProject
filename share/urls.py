908f678a258605cc71b874f892ce3820cb9b32e0
Ifeanyijasper <ifeanyijasper11@gmail.com>
2021-03-02 17:11:28 +0100

    build hopefully

--- share/urls.py
+++ share/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+router.register('register', views.RegisterViewSet)
+router.register('product', views.ProductsViewSet)
+router.register('service', views.ServicesViewSet)
+router.register('purchase', views.PurchasesViewSet)
 app_name = 'share'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', include(router.urls)),
 
 ]
