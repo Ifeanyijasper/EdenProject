33c5f2350a9e2e1f6f5e8ebe6b3c56ee87461f7c
Ifeanyijasper <ifeanyijasper11@gmail.com>
2021-03-16 20:12:03 +0100

    version

--- salon/urls.py
+++ salon/urls.py
@@ -44,5 +44,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path('.*', TemplateView.as_view(template_name='index.html'))]
