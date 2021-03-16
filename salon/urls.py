c670e05aa546527b140a345be6c931dd4b373ff9
Ifeanyijasper <ifeanyijasper11@gmail.com>
2021-03-16 19:58:40 +0100

    version

--- salon/urls.py
+++ salon/urls.py
@@ -42,7 +42,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path('.*', TemplateView.as_view(template_name='index.html'))]
