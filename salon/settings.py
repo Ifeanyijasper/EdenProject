8d07d47f2444dea7a58962bda840adf027d29ede
Ifeanyijasper <ifeanyijasper11@gmail.com>
2021-07-01 09:44:56 +0100

    storage editing

--- salon/settings.py
+++ salon/settings.py
@@ -157,10 +157,10 @@ USE_TZ = True
 BASE_DIR     = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/build/static/'
-MEDIA_URL =  '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL =  '/build/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "build/static/media")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'build/static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
