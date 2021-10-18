97ccf66ae6b9ef3e89bb91b3d74b02256be7ac93
Jumeb <bricejume@gmail.com>
2021-10-18 22:14:22 +0100

    updates

--- src/components/Notification/Confirmation.component.js
+++ src/components/Notification/Confirmation.component.js
@@ -23,7 +23,7 @@ const Confirmation = (props) => {
                     onClick={(e) => stop(e)}
                     className={`absolute capitalize shadow-xl p-4 w-96 text-wrap text-center font-semibold rounded-md bg-green-700 bg-opacity-90 left-1/2 top-1/2 transform -translate-y-1/2 -translate-x-1/2 text-white z-40 text-sm transition-all duration-500 ease-in-out`}
                 >
-                    <h2>{data?.msg} <br /> {data?.data?.fullname ? data?.data?.fullname : data?.data?.username}{data?.data?.name} {data?.data?.client && `Purchase by ${data?.data?.client} served by ${data?.data?.worker}. Total: ${Thousand(data?.data?.total)} XAF`}</h2>
+                    <h2>{data?.msg} <br /> {data?.data?.fullname ? data?.data?.fullname : data?.data?.username}{data?.data?.name} {data?.data?.client && data?.data?.worker && `Purchase by ${data?.data?.client} served by ${data?.data?.worker}.`} <br /> {data?.data?.client && data?.data?.worker && `Total: ${Thousand(data?.data?.total)} XAF`}</h2>
                     <div className="flex items-center justify-end mt-3">
                         <button onClick={() => onClick(data?.data?.id)} className={`outline-none mx-2 px-2 py-1 rounded ${data?.suspend ? 'bg-yellow-400' : 'bg-red-500'}`}>{loading ? 'Deleting' : 'Confirm'}</button>
                         <button onClick={() => setConfirm(false)} className="outline-none mx-2 px-2 py-1 rounded bg-blue-400">Cancel</button>
