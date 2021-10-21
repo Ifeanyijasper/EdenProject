d0c773a32a8ed3abd3c0d5e3383162de8aa00d72
Jumeb <bricejume@gmail.com>
2021-10-21 18:21:29 +0100

    updates

--- src/components/TestimonialCard/TestimonialCard.component.js
+++ src/components/TestimonialCard/TestimonialCard.component.js
@@ -7,11 +7,11 @@ const TestimonialCard = (props) => {
         <div class="relative h-48 md:flex bg-gray-100 rounded-lg p-8 md:p-0 md:px-4 w-full md:w-120">
             {testimony.img ?
                 <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> :
-                <h2 className={`w-32 h-32 md:w-48 md:h-auto text-gray-500 md:rounded-none rounded-full mx-auto text-7xl flex items-center justify-center tracking-wider`}>{extractInitials(testimony.client_name)}</h2>
+                <h2 className={`w-32 h-32 md:w-48 md:h-auto text-primary md:rounded-none rounded-full mx-auto text-7xl flex items-center justify-center tracking-wider`}>{extractInitials(testimony.client_name)}</h2>
             }
             <div class="pt-4 md:p-6 text-center md:text-left space-y-4">
                 <blockquote>
-                    <p class="text-base text-gray-700 font-semibold">
+                    <p class="text-base text-gray-800 font-semibold">
                         “{testimony.testimonial.substr(0, 250)}”
                     </p>
                 </blockquote>
