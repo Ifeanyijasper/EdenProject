0d55123678f7668962f83a3e11b6a9a9ad180a35
Jumeb <bricejume@gmail.com>
2021-10-25 17:15:03 +0100

    updates

--- src/components/Testimonial/Testimonial.component.js
+++ src/components/Testimonial/Testimonial.component.js
@@ -6,18 +6,18 @@ const Testimonial = (props) => {
   const { testimonial } = props;
   return (
       
-    <div class="relative h-48 md:flex bg-gray-100 rounded-lg p-8 md:p-0 md:px-4 w-full md:w-120">
+    <div class="relative h-auto md:h-52 md:flex bg-gray-100 rounded-lg px-6 py-4 md:p-8 md:px-4 w-full md:w-120">
       {testimonial.img ?
-        <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> :
-        <h2 className={`w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto text-7xl flex items-center justify-center tracking-wider`}>{extractInitials(testimonial.client_name)}</h2>
+        <img class="w-28 h-28 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> :
+        <h2 className={`w-28 h-28 md:w-48 md:h-auto text-primary md:rounded-none rounded-full mx-auto text-5xl md:text-7xl flex items-center justify-center tracking-wider`}>{extractInitials(testimonial.client_name)}</h2>
       }
-      <div class="pt-4 md:p-6 text-center md:text-left space-y-4">
+      <div class="pt-4 md:p-6 md:pt-4 text-center md:text-left space-y-4">
         <blockquote>
           <p class="text-base font-semibold">
             “{testimonial.testimonial.substr(0, 250)}”
           </p>
         </blockquote>
-        <figcaption class="absolute bottom-3 right-3 font-medium text-sm">
+        <figcaption class="text-right md:absolute md:bottom-3 md:right-3 font-medium text-sm">
           <div class="text-cyan-600">
             {testimonial.client_name}
           </div>
--- src/components/TestimonialCard/TestimonialCard.component.js
+++ src/components/TestimonialCard/TestimonialCard.component.js
@@ -4,18 +4,18 @@ import extractInitials from '../../utils/extractIni';
 const TestimonialCard = (props) => {
     const { testimony } = props;
     return (
-        <div class="relative h-48 md:flex bg-gray-100 rounded-lg p-8 md:p-0 md:px-4 w-full md:w-120">
+        <div class="relative h-auto md:h-52 md:flex bg-gray-100 rounded-lg px-6 py-4 md:p-8 md:px-4 w-full md:w-120">
             {testimony.img ?
-                <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> :
-                <h2 className={`w-32 h-32 md:w-48 md:h-auto text-primary md:rounded-none rounded-full mx-auto text-7xl flex items-center justify-center tracking-wider`}>{extractInitials(testimony.client_name)}</h2>
+                <img class="w-28 h-28 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> :
+                <h2 className={`w-28 h-28 md:w-48 md:h-auto text-primary md:rounded-none rounded-full mx-auto text-5xl md:text-7xl flex items-center justify-center tracking-wider`}>{extractInitials(testimony.client_name)}</h2>
             }
-            <div class="pt-4 md:p-6 text-center md:text-left space-y-4">
+            <div class="pt-0 md:p-6 md:pt-4 text-center md:text-left space-y-2 md:space-y-4">
                 <blockquote>
                     <p class="text-base text-gray-800 font-semibold">
                         “{testimony.testimonial.substr(0, 250)}”
                     </p>
                 </blockquote>
-                <figcaption class="absolute bottom-3 right-3 font-medium text-sm">
+                <figcaption class="text-right md:absolute md:bottom-3 md:right-3 font-medium text-sm">
                     <div class="text-gray-700">
                         {testimony.client_name}
                     </div>
